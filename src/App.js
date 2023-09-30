4b20a4baa4020e918c6ea069877e06e85be4c055
r3dxm <moham3d.maaxin@gmail.com>
2023-09-30 23:35:43 +0530

    push to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./images/logo.svg";
+import Illustration from "./images/illustration-mockups.svg";
+import {
+	faFacebookF,
+	faTwitter,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="illustrationContainer">
+				<img className="logo" src={Logo} alt="logo" />
+				<img className="illustration" src={Illustration} alt="illustration" />
+				<div className="empty" />
+			</div>
+			<div className="infoContainer">
+				<div className="empty" />
+				<div className="textContainer">
+					<h1>Build The Community Your Fans Will Love</h1>
+					<p>
+						Huddle re-imagines the way we build communities. You have a voice,
+						but so does your audience. Create connections with your users as you
+						engage in genuine discussion.
+					</p>
+					<button className="registerButton">Register</button>
+				</div>
+				<div className="socials">
+					<button className="socialButton">
+						<FontAwesomeIcon className="socialIcon" icon={faFacebookF} />
+					</button>
+					<button className="socialButton">
+						<FontAwesomeIcon className="socialIcon" icon={faTwitter} />
+					</button>
+					<button className="socialButton">
+						<FontAwesomeIcon className="socialIcon" icon={faInstagram} />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
